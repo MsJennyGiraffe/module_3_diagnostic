7d384694e6999c171058f489f25be9e8512b29c6
Jennifer <jennifersoden@hotmail.com>
2016-09-12 09:50:40 -0600

    Finds closest stations by zipcode

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,12 +1,16 @@
 # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&api_key=ENV["NREL_KEY"]&radius=6
 class NrelService
   def initialize
-
+    @conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json")
   end
 
   def get_by_zipcode(zipcode)
-    conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json")
-    response = conn.get("?location=80203&api_key=ENV['NREL_KEY']&radius=6")
-    JSON.parse(response.body)
+    response = @conn.get("?location=80203&api_key=#{ENV['NREL_KEY']}&radius=6")
+    parse(response.body)["fuel_stations"]
   end
+
+  private
+    def parse(response)
+      JSON.parse(response)
+    end
 end
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -5,9 +5,13 @@ describe NrelService do
     it "returns a list stations by location" do
       VCR.use_cassette("stations") do
         locations = NrelService.new.get_by_zipcode(80203)
-        location = location.first
+        location = locations.first
 
-        expect(location).to eq("")
+        expect(location["station_name"]).to eq("UDR")
+        expect(location["street_address"]).to eq("800 Acoma St")
+        expect(location["fuel_type_code"]).to eq("ELEC")
+        expect(location["distance"]).to eq(0.3117)
+        expect(location["access_days_time"]).to eq("24 hours daily")
       end
     end
   end
